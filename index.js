4ab2526e03e04c87bef8bd8bb8a78dc843564752
matchacookie <tongkhaiminh@gmail.com>
2022-09-10 16:45:59 +0700

    Add function to start button

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-    
+const game = document.querySelector('.game');
+
+const startGame = () => {
+	const foreground = document.createElement('div');
+	const p1_text = document.createElement('div');
+	const p2_text = document.createElement('div');
+	foreground.classList.add('foreground');
+
+	game.append(foreground);
+};
+const start = document.querySelector('.start');
+start.addEventListener('click', () => startGame());
