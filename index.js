7b9d072d36461078bce9276d2d524d61e5fb37f5
matchacookie <tongkhaiminh@gmail.com>
2022-09-10 20:25:27 +0700

    Add player text and score to game start function, change game start function name

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const game = document.querySelector('.game');
 
-const startGame = () => {
+const newGame = () => {
 	const foreground = document.createElement('div');
 	const p1_text = document.createElement('div');
+	const p1_score = document.createElement('div');
 	const p2_text = document.createElement('div');
+	const p2_score = document.createElement('div');
 	foreground.classList.add('foreground');
 
+	p1_text.textContent = 'player 1';
+	p1_score.textContent = 0;
+
+    p1_score.classList.add('.')
+
+	p2_text.textContent = 'player 2';
+	p2_score.textContent = 0;
+
+	foreground.append(p1_text);
+	foreground.append(p1_score);
+	foreground.append(p2_text);
+	foreground.append(p2_score);
 	game.append(foreground);
 };
 const start = document.querySelector('.start');
-start.addEventListener('click', () => startGame());
+start.addEventListener('click', () => newGame());
