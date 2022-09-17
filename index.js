3b43513251f9865bb2b5799b08a9d62921006ba8
matchacookie <tongkhaiminh@gmail.com>
2022-09-17 10:36:42 +0700

    Style player text and score

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ const game = document.querySelector('.game');
 
 const newGame = () => {
 	const foreground = document.createElement('div');
+	const text = document.createElement('div');
 	const p1_text = document.createElement('div');
 	const p1_score = document.createElement('div');
 	const p2_text = document.createElement('div');
 	const p2_score = document.createElement('div');
+
 	foreground.classList.add('foreground');
+	text.classList.add('game-text');
 
 	p1_text.textContent = 'player 1';
 	p1_score.textContent = 0;
 
-    p1_score.classList.add('.')
-
 	p2_text.textContent = 'player 2';
 	p2_score.textContent = 0;
 
-	foreground.append(p1_text);
-	foreground.append(p1_score);
-	foreground.append(p2_text);
-	foreground.append(p2_score);
+	p1_score.classList.add('score');
+	p1_text.classList.add('player-text');
+
+	p2_score.classList.add('score');
+	p2_text.classList.add('player-text');
+
+	p1_text.append(p1_score);
+	p2_text.append(p2_score);
+
+	text.append(p1_text);
+	text.append(p2_text);
+
+	foreground.append(text);
+
 	game.append(foreground);
 };
+
 const start = document.querySelector('.start');
-start.addEventListener('click', () => newGame());
+start.addEventListener('click', () => newGame(), { once: true });
